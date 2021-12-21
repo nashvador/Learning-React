00b099218524d16aa27d311ddac6a81966c9d976
Nash Vador <nashvador@gmail.com>
2021-12-20 22:36:35 -0500

    Trying to upload changes for the first time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState, useEffect } from "react";
+import "./App.css";
+import "./components/imagefile.css";
+
+const images = [
+  "https://images.unsplash.com/photo-1559627755-42212e5c5fdf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+  "https://images.unsplash.com/photo-1535182463927-440364075d9c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80",
+  "https://images.unsplash.com/photo-1559627755-42212e5c5fdf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+  "https://images.unsplash.com/photo-1505459668311-8dfac7952bf0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  "https://images.unsplash.com/photo-1459745930869-b3d0d72c3cbb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [activeIndex, setActiveIndex] = React.useState(0);
+  console.log(useState(0));
+
+  const handleOnClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  const image = images.map((el, idx) => {
+    return (
+      <div className="container">
+        <img
+          src={el}
+          onClick={() => handleOnClick(idx)}
+          className={activeIndex === idx ? "active" : "hidden"}
+        />
+      </div>
+    );
+  });
+  return <div className="body">{image}</div>;
 }
 
 export default App;
