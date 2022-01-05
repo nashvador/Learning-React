6628d29b0736c00c0bec7b14f2d31f253a116ea3
Nash Vador <nashvador@gmail.com>
2022-01-05 13:56:13 -0500

    Adding Color hover over, not sure what to do next.

--- etch-a-sketch/src/components/Box.js
+++ etch-a-sketch/src/components/Box.js
@@ -3,8 +3,8 @@ import App from "../App";
 
 export default function Box(props) {
   const [activeIndex, setActiveIndex] = useState(0);
-  const [colorHover, setColorHover] = useState();
-  //  index
+  // const [colorHover, setColorHover] = useState();
+  // console.log(arrayBoxes);
 
   const styles = {
     width: "560px",
@@ -25,6 +25,12 @@ export default function Box(props) {
     setActiveIndex(id);
     // console.log(activeIndex);
     const randomColor = () => Math.floor(Math.random() * 255);
+    // setColorHover(arrayBoxes);
+    // let temp_state = [...colorHover];
+    // let temp_element = { ...temp_state[id] };
+    // temp_element.hoverOver = temp_element.hoverOver + true;
+    // temp_state[id] = temp_element;
+    // setColorHover(temp_state);
   }
 
   let arrayBoxes = useMemo(() => {
@@ -37,7 +43,7 @@ export default function Box(props) {
   }, [props.boxSize]);
 
   // Have an object in the for loop with a colored property and then when you hover - set it to a rgb value,
-
+  console.log(arrayBoxes);
   return (
     <div style={styles}>
       {arrayBoxes.map((el, l) => {
