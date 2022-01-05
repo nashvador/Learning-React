7ea8fe7062840dc1e23097bb0a6a905f601156e8
Nash Vador <nashvador@gmail.com>
2022-01-05 15:50:11 -0500

    adding some inline styles, and adding an id.

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,7 +1,35 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [clickFunction, setClickfunction] = React.useState();
+  const styles = {
+    border: "1px solid black",
+    display: "flex",
+    fontSize: "90px",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+  let tictactoeArray = [];
+  for (let i = 0; i < 9; i++) {
+    tictactoeArray.push({ id: i, clicked: false });
+  }
+
+  // setClickfunction(tictactoeArray);
+  // function handleClick(id) {
+  //   setClickfunction((prevState) => ({
+  //     ...prevState,
+  //     clicked: clicked.id === id ? {}
+  //   }));
+  // }
+
+  return (
+    <div className="grid">
+      {tictactoeArray.map((el, i) => (
+        <div style={styles} key={i}></div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
---
+++ tic-tac-toe/src/components/grid.js
@@ -0,0 +1,3 @@
+export default function grid() {
+  return <div></div>;
+}
