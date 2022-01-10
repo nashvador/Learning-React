36391fc99dccaacbaeb32d3dcc458fa2940f8c5e
Nash Vador <nashvador@gmail.com>
2022-01-10 13:18:50 -0600

    Added buttons

--- calculator/src/components/calculationbuttons.js
+++ calculator/src/components/calculationbuttons.js
@@ -10,24 +10,26 @@ export default function Buttons() {
   };
   return (
     <div className="calculator-grid" style={styles}>
-      <div style={{ gridColumn: "1/4" }}>C</div>
-      <div>Backspace</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>+</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>+</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>+</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>+</div>
+      <button style={{ gridColumn: "1/3" }}>C</button>
+      <button>Backspace</button>
+      <button>=</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>+</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>-</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>X</button>
+      <button style={{ gridColumn: "1/3" }}>0</button>
+      <button>.</button>
+      <button>
+        <span>&#247;</span>
+      </button>
     </div>
   );
 }
