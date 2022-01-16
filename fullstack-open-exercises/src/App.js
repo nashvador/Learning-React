a23e70fe0c78e2428b7daf7d02a4b8e40c44e744
Nash Vador <nashvador@gmail.com>
2022-01-15 21:15:15 -0600

    Exercise on Fullstackopen

--- fullstack-open-exercises/src/App.js
+++ fullstack-open-exercises/src/App.js
@@ -1,52 +1,38 @@
-import Course from "./components/Course";
+import React, { useState } from "react";
 
 const App = () => {
-  const courses = [
-    {
-      name: "Half Stack application development",
-      id: 1,
-      parts: [
-        {
-          name: "Fundamentals of React",
-          exercises: 10,
-          id: 1,
-        },
-        {
-          name: "Using props to pass data",
-          exercises: 7,
-          id: 2,
-        },
-        {
-          name: "State of a component",
-          exercises: 14,
-          id: 3,
-        },
-        {
-          name: "Redux",
-          exercises: 11,
-          id: 4,
-        },
-      ],
-    },
-    {
-      name: "Node.js",
-      id: 2,
-      parts: [
-        {
-          name: "Routing",
-          exercises: 3,
-          id: 1,
-        },
-        {
-          name: "Middlewares",
-          exercises: 7,
-          id: 2,
-        },
-      ],
-    },
-  ];
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
 
-  return <Course courses={courses} />;
+  const handleChangeValue = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const clickFunction = (event) => {
+    event.preventDefault();
+
+    setPersons((prevPersons) => [...prevPersons, { name: newName }]);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input onChange={handleChangeValue} />
+        </div>
+        <div>
+          <button onClick={clickFunction} type="submit">
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((names, i) => (
+        <p key={i}>{names.name}</p>
+      ))}
+    </div>
+  );
 };
 
 export default App;
