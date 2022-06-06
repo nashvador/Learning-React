3659035548023d69d044c5d767733e4a8ec7738e
nashvador <nashvador@gmail.com>
2022-06-05 20:06:43 -0500

    fullstackopen

---
+++ fullstack-open-exercises/part1/part1-coursemat/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  console.log('Hello from component')
+  return  (<div>
+    <p>Hello world</p>
+    </div>)
+  
+}
+
+export default App;
