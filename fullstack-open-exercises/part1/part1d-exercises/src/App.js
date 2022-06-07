1fae4cd517106ba2d1ac76f3785bb279f8299709
nashvador <nashvador@gmail.com>
2022-06-07 16:16:49 -0500

    React anecdote solutions

--- fullstack-open-exercises/part1/part1d-exercises/src/App.js
+++ fullstack-open-exercises/part1/part1d-exercises/src/App.js
@@ -1,40 +1,49 @@
 import { useState } from 'react'
 
-const Good = ({good, bad, neutral, all}) => {
-return (
-  <div>
-  <p>good {good}</p>
-  <p>neutral {neutral}</p>
-  <p>bad {bad}</p>
-  <p>all {all}</p>
-  <p>average {(good-bad)/all}</p>
-  <p>average {good/all*100}%</p>
-  </div>
-)
-}
-
-const Button = ({setBad, setGood, setNeutral}) => {
-
-  return( <div>  <button onClick={() => setGood((count) => count +1)}>good</button>
-  <button onClick={() => setNeutral((count) => count +1)}>neutral</button>
-  <button onClick={() => setBad((count) => count +1)}>bad</button>
-  </div>)
-  }
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const all = bad+good+neutral
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [vote, setVotes] = useState({0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0}) 
+  let copy = {...vote}
+  let values = Object.values(vote)
+  let max = Math.max(...values)
+  let index = values.indexOf(max)
 
+  console.log(index)
+
+  const voteFunction = () => {
+    copy[selected] += 1
+    setVotes(copy)
+  }
+   const clickFunction = () => {
+   let value = Math.floor(Math.random() * 7)
+   setSelected(value)
+   } 
+
+   console.log(vote)
 
   return (
     <div>
-    <h1>give feedback</h1>
-<Button setBad={setBad} setGood={setGood} setNeutral={setNeutral} />
-  <h1>Stats</h1>
-    {all ==0 ? <p>No feedback given</p> :<Good good={good} bad={bad} neutral={neutral} all={all}/>}
+      <h1>Anecdote of the day</h1>
+
+      <p>{anecdotes[selected]}</p>
+      <button onClick={voteFunction}>Vote</button>
+      <button onClick={clickFunction}>next anecdote</button>
+      <p>This has {vote[selected]} votes</p>
+
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[index]}</p>
+      <p>has {max} votes</p>
     </div>
   )
 }
