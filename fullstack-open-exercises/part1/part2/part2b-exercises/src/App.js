8b37014ca34d2dcbd0201d5a4adc3421845d725b
nashvador <nashvador@gmail.com>
2022-06-11 16:11:12 -0500

    2.7 Solutions

--- fullstack-open-exercises/part1/part2/part2b-exercises/src/App.js
+++ fullstack-open-exercises/part1/part2/part2b-exercises/src/App.js
@@ -10,24 +10,32 @@ const handlePhoneChange = (event) => {
   setNewName(event.target.value)
 }
 
-const saveNmae = () => {
-  setPersons((prevPersons)=>({...prevPersons, newName}))
+const onClick = () => {
+let phoneBool = false
+persons.find(person => person.name == newName ? phoneBool = true : phoneBool = false)
+if (phoneBool) {
+  window.alert(`${newName} is already in the phonebook`)
+} else setPersons((prevPersons) => ([...prevPersons, {name: newName}]) ) 
+}
 
+
+const preventReset = (event) => {
+  event.preventDefault()
 }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={preventReset}>
         <div>
           name: <input value={newName} onChange={handlePhoneChange} />
         </div>
         <div>
-          <button type="submit" onClick={(saveNmae)}>add</button>
+          <button type="submit" onClick={onClick}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(persons => <p>{persons.name}</p>)}
+      {persons.map((personElements)=><p>{personElements.name}</p>)}
     </div>
   )
 }
