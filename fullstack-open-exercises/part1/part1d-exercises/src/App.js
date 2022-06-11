1351d7c111ea227680147e68aa5081d677efe248
nashvador <nashvador@gmail.com>
2022-06-11 00:10:50 -0500

    space

--- fullstack-open-exercises/part1/part1d-exercises/src/App.js
+++ fullstack-open-exercises/part1/part1d-exercises/src/App.js
@@ -3,7 +3,7 @@ import { useState } from 'react'
 
 const App = () => {
   const anecdotes = [
-    'If it hurts, do it more often',
+    'If it hurts, do it more often ',
     'Adding manpower to a late software project makes it later!',
     'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
