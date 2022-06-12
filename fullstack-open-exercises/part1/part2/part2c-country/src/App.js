9dcdfe21941644ea6c0950b0cc4fe90932e1a33e
nashvador <nashvador@gmail.com>
2022-06-12 15:24:33 -0500

    Installing axios and country data

--- fullstack-open-exercises/part1/part2/part2c-country/src/App.js
+++ fullstack-open-exercises/part1/part2/part2c-country/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+
+import axios from "axios";
+
+
 
 function App() {
+const [countryData, setCountryData] = useState([])
+// Ask david if we should put usefecct in const
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
     </div>
   );
 }
