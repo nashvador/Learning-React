a982d517493bb969310acd3d4a1b0ddc3623787a
nashvador <nashvador@gmail.com>
2022-06-12 19:18:04 -0500

    2.12 Solutions

--- fullstack-open-exercises/part1/part2/part2c-country/src/App.js
+++ fullstack-open-exercises/part1/part2/part2c-country/src/App.js
@@ -6,14 +6,43 @@ import axios from "axios";
 
 function App() {
 const [countryData, setCountryData] = useState([])
+const [countrySearch, setCountrySearch] = useState('')
 // Ask david if we should put usefecct in const
+// Why do I have to add a return statement
 
+useEffect(()=> {
+axios.get('https://restcountries.com/v3.1/all').then(response => setCountryData(response.data))
+}, [])
 
+const onCountrySearch = (event) => {
+  setCountrySearch(event.target.value)
+}
 
+let filteredData = countryData.filter((newData) => newData.name.common.toLowerCase().includes(countrySearch.toLowerCase()))
+
+const filterCountryData = () => {
+if (filteredData.length > 10) {
+  return <div>Too many countries</div>
+} else if (filteredData.length <10 && filteredData.length > 1) {
+  return filteredData.map(countries => <div>{countries.name.common}</div>)
+} else if (filteredData.length == 1) {
+  console.log(filteredData[0])
+  return (<div>
+    <h1>{filteredData[0].name.common}</h1>
+    <p>capital {filteredData[0].capital}</p>
+    <p>area {filteredData[0].area}</p>
+    <h2>languages</h2>
+    <ul>{(Object.values(filteredData[0].languages)).map(language => <li>{language}</li>)}</ul>
+    <img src={filteredData[0].flags.png}></img>
+  </div>)
+
+}
+}
 
   return (
     <div>
-
+      find countries <input value={countrySearch} onChange={onCountrySearch}></input>
+      {filterCountryData()}
     </div>
   );
 }
