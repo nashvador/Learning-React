0118b1fd1056ddb7983897ca4e34609483f11ed2
nashvador <nashvador@gmail.com>
2022-06-14 22:35:33 -0500

    2.13 solution

--- fullstack-open-exercises/part1/part2/part2c-country/src/App.js
+++ fullstack-open-exercises/part1/part2/part2c-country/src/App.js
@@ -2,13 +2,34 @@ import { useState, useEffect } from "react";
 
 import axios from "axios";
 
+const NewCountry = ({countries}) => {
+  const [show, setShow] = useState(true)
+const displayStyle = {
+  display: "none"
+}
+
+const notDisplayStyle = {
+  display: "inherit"
+}
+return( 
+  <div>{countries.name.common}<button onClick={() => setShow(oldShow => !oldShow)}>shown</button>
+   <div style={show ? displayStyle : notDisplayStyle }>
+  <h1>{countries.name.common}</h1>
+    <p>capital {countries.capital}</p>
+    <p>area {countries.area}</p>
+    <h2>languages</h2>
+    <ul>{(Object.values(countries.languages)).map(language => <li>{language}</li>)}</ul>
+    <img src={countries.flags.png}></img>
+  </div>
+  </div>
+)  
+} 
 
 
 function App() {
 const [countryData, setCountryData] = useState([])
 const [countrySearch, setCountrySearch] = useState('')
-// Ask david if we should put usefecct in const
-// Why do I have to add a return statement
+
 
 useEffect(()=> {
 axios.get('https://restcountries.com/v3.1/all').then(response => setCountryData(response.data))
@@ -18,22 +39,29 @@ const onCountrySearch = (event) => {
   setCountrySearch(event.target.value)
 }
 
+
+
+
+
+
 let filteredData = countryData.filter((newData) => newData.name.common.toLowerCase().includes(countrySearch.toLowerCase()))
 
 const filterCountryData = () => {
 if (filteredData.length > 10) {
   return <div>Too many countries</div>
 } else if (filteredData.length <10 && filteredData.length > 1) {
-  return filteredData.map(countries => <div>{countries.name.common}</div>)
+
+  return filteredData.map((countries) =>  <div> <NewCountry countries={countries} />
+  </div>)
 } else if (filteredData.length == 1) {
-  console.log(filteredData[0])
+  let singleCountryData = filteredData[0]
   return (<div>
-    <h1>{filteredData[0].name.common}</h1>
-    <p>capital {filteredData[0].capital}</p>
-    <p>area {filteredData[0].area}</p>
+    <h1>{singleCountryData.name.common}</h1>
+    <p>capital {singleCountryData.capital}</p>
+    <p>area {singleCountryData.area}</p>
     <h2>languages</h2>
-    <ul>{(Object.values(filteredData[0].languages)).map(language => <li>{language}</li>)}</ul>
-    <img src={filteredData[0].flags.png}></img>
+    <ul>{(Object.values(singleCountryData.languages)).map(language => <li>{language}</li>)}</ul>
+    <img src={singleCountryData.flags.png}></img>
   </div>)
 
 }
