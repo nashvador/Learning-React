a1434820bd56510ba7b96d43ab62c77dffae6a7e
nashvador <nashvador@gmail.com>
2022-07-14 15:49:43 -0400

    completed router set up

--- fullstack-open-exercises/part4/index.js
+++ fullstack-open-exercises/part4/index.js
@@ -1,38 +1,10 @@
-const {app} = require(`./app`)
-const {MONGODB_URI, PORT} = require('./utils/config')
-const logger = require('./utils/logger')
+const app = require('./app') // the actual Express application
 const http = require('http')
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
 const server = http.createServer(app)
-const Blog = require('./models/blog')
 
-
-
-mongoose.connect(MONGODB_URI)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-server.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
