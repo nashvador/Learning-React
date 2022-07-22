dcdf65b16eba872a91ed8f206cf4afeff5ebb536
nashvador <nashvador@gmail.com>
2022-07-22 14:55:19 -0400

    Add tokenExtractor to middleware

--- fullstack-open-exercises/part4/controllers/blogs.js
+++ fullstack-open-exercises/part4/controllers/blogs.js
@@ -4,14 +4,6 @@ const Blog = require("../models/blog");
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 
-const getTokenFrom = (request) => {
-  const authorization = request.get("authorization");
-  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
-    return authorization.substring(7);
-  }
-  return null;
-};
-
 blogsRouter.get("/", async (request, response) => {
   const getBlogs = await Blog.find({}).populate("user", {
     username: 1,
@@ -32,8 +24,7 @@ blogsRouter.get("/:id", async (request, response, next) => {
 
 blogsRouter.post("/", async (request, response, next) => {
   const body = request.body;
-  const token = getTokenFrom(request);
-  const decodedToken = jwt.verify(token, process.env.SECRET);
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
   if (!decodedToken.id) {
     return response.status(401).json({ error: "token missing or invalid" });
   }
