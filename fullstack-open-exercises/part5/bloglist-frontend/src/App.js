b7466f0dbab89b5e4f767d7e69e82f2e5385fd8b
nashvador <nashvador@gmail.com>
2022-07-25 16:04:53 -0400

    Add bloglist frontend

--- fullstack-open-exercises/part5/bloglist-frontend/src/App.js
+++ fullstack-open-exercises/part5/bloglist-frontend/src/App.js
@@ -1,76 +1,21 @@
-import axios from "axios";
 import { useState, useEffect } from "react";
 import Blog from "./components/Blog";
+import blogService from "./services/blogs";
 import loginService from "./services/login";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [user, setUser] = useState(null);
-  const [specificBlog, setSpecificBlog] = useState([]);
 
   useEffect(() => {
-    axios.get("api/blogs").then((blogs) => setBlogs(blogs));
+    blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
-
-  console.log(blogs);
-
-  const handleLogin = async (event) => {
-    event.preventDefault();
-
-    try {
-      const user = await loginService.login({
-        username,
-        password,
-      });
-      setUser(user);
-      setUsername("");
-      setPassword("");
-    } catch (exception) {
-      setErrorMessage("Wrong credentials");
-      setTimeout(() => {
-        setErrorMessage(null);
-      }, 5000);
-    }
-  };
-
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-        <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password
-        <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>
-  );
-
-  const displayBlogs = () => {
-    const getBlogsByName = blogs.map((eachBlog) => console.log(eachBlog.user));
-  };
 
   return (
     <div>
-      {user === null ? loginForm() : displayBlogs()}
       <h2>blogs</h2>
-      {/* {blogs.map((blog) => (
+      {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
-      ))} */}
+      ))}
     </div>
   );
 };
