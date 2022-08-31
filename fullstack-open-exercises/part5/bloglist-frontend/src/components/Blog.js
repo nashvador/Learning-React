bc0d0aa309816b5ea88364f8b0dceec4398049fa
nashvador <nashvador@gmail.com>
2022-08-31 18:43:20 -0400

    Add 5.7 solutions

--- fullstack-open-exercises/part5/bloglist-frontend/src/components/Blog.js
+++ fullstack-open-exercises/part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,33 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [showBlog, setShowBlog] = useState(false);
+  console.log(blog);
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setShowBlog((oldstate) => !oldstate)}>
+          {showBlog ? "hide" : "show"}
+        </button>
+        <div style={showBlog ? { display: "" } : { display: "none" }}>
+          <div>{blog.url}</div>
+          <div>
+            {blog.likes} <button>like</button>
+          </div>
+          {blog.user.name}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
