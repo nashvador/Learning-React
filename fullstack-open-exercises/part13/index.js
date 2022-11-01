06f7b01ffa18a0add293834e6ff7a0b6e5e56b21
nashvador <nashvador@gmail.com>
2022-11-01 09:38:09 -0400

    13.16 solutions

--- fullstack-open-exercises/part13/index.js
+++ fullstack-open-exercises/part13/index.js
@@ -8,6 +8,7 @@ const { connectToDatabase } = require("./util/db");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
 const loginRouter = require("./controllers/login");
+const authorRouter = require("./controllers/authors");
 
 app.use(express.json());
 app.use(middleware.blogFinder);
@@ -15,6 +16,7 @@ app.use(middleware.blogFinder);
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
+app.use("/api/author", authorRouter);
 
 const start = async () => {
   await connectToDatabase();
